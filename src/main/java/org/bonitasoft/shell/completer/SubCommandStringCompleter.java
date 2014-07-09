7bf26277e20f5f9b7454bc6c01bac25cc6be77ed
Antoine Mottier <antoine.mottier@bonitasoft.com>
2014-07-09 14:51:31 +0200

    Temp commit of src/main/java/org/bonitasoft/shell/completer/SubCommandStringCompleter.java

--- src/main/java/org/bonitasoft/shell/completer/SubCommandStringCompleter.java
+++ src/main/java/org/bonitasoft/shell/completer/SubCommandStringCompleter.java
@@ -14,6 +14,27 @@ public class SubCommandStringCompleter implements BonitaCompleter {
 
     @Override
     public int complete(ArgumentParser commandLine, List<CharSequence> candidates) {
+        ShellCommand command = searchCommandStartingWith(commandLine);
+        if (command != null) {
+            return searchSubCommandCompleterToUse(commandLine, command).complete(commandLine, candidates);
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public CompletionHelper getCompletionHelper() {
+        return new CompletionHelper() {
+
+            @Override
+            public String getHelp(ArgumentParser argumentParser) {
+                ShellCommand command = searchCommandStartingWith(argumentParser);
+                return searchSubCommandCompleterToUse(argumentParser, command).getCompletionHelper().getHelp(argumentParser);
+            }
+        };
+    }
+
+    ShellCommand searchCommandStartingWith(ArgumentParser commandLine) {
         // Get sub command name (firsts chars) typed by user
         String firstArgument = commandLine.getFirstArgument();
 
@@ -22,16 +43,15 @@ public class SubCommandStringCompleter implements BonitaCompleter {
             // Search sub command match user typing
             if (shellCommand.getName().startsWith(firstArgument)) {
                 // Call command completer that match the argument number
-                return shellCommand.getCompleters().get(commandLine.getLastArgumentIndex() - 1).complete(commandLine, candidates);
+                return shellCommand;
             }
         }
 
-        return 0;
+        return null;
     }
 
-    @Override
-    public CompletionHelper getCompletionHelper() {
-        return null;
+    static BonitaCompleter searchSubCommandCompleterToUse(ArgumentParser commandLine, ShellCommand command) {
+        return command.getCompleters().get(commandLine.getLastArgumentIndex() - 1);
     }
 
 }
